b26e0793b086e077c5f8926bcb8f5593ba960ee9
TBM <tbm@TBMs-MacBook-Pro.local>
2018-06-22 08:51:46 +0700

    update phat 1

---
+++ LichVanNien/ChiTietViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LichVanNien
+//
+//  Created by TBM on 6/21/18.
+//  Copyright © 2018 TBM. All rights reserved.
+//
+
+import UIKit
+
+class ChiTietViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
